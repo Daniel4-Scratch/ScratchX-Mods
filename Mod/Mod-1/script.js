21870af1ee3af91d9184e1d594e624a5f9be8885
Daniel <65277548+Daniel4-Scratch@users.noreply.github.com>
2020-08-12 17:44:01 +1000

    Create script.js

---
+++ Mod/Mod-1/script.js
@@ -0,0 +1,37 @@
+(function(ext) {
+    ext._shutdown = function() {};
+    ext._getStatus = function() {
+        return {status: 2, msg: 'Ready'};
+    };
+    ext.alert = function(text) {
+        alert(text);
+    };
+
+    ext.confirm = function(text) {
+        return confirm(text);
+    };
+    ext.prompt = function(text) {
+        var output = prompt(text);
+        if (!output) {
+            return '';
+        } else {
+            return output;
+        };
+    };
+    ext.log = function(text) {
+        console.log(text);
+        };
+    ext.url = function(url) {
+        window.open(url);
+        };
+    var descriptor = {
+        blocks: [
+            [' ','alert %s','alert'],
+            ['b','confirm %s','confirm'],
+            ['r','prompt %s','prompt'],
+            [' ','console log %s','log'],
+            [' ','open url %s','url'],
+        ]
+     };
+    ScratchExtensions.register('ScratchJS', descriptor, ext);
+})({});
